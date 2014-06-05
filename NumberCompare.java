35b54e01c2fd09f985c1b218cef197d7375faebd
Austin Lasota <austinlasota@gmail.com>
2014-06-05 10:09:37 -0400

    Update NumberCompare.java

--- NumberCompare.java
+++ NumberCompare.java
@@ -1,27 +1,24 @@
-/* package whatever; // don't place package name! */
-import java.util.scanner;
-
+import java.util.Scanner;
 
 public class NumberCompare
 {
-	public static void main (String[] args) throws java.lang.Exception
+    public static void main (String[] args)
 	{
-		double first. second;
-		Scanner user_input = new Scanner(System.in); 
+		double first, second;
+		Scanner userinput = new Scanner(System.in); 
 		System.out.println("Type the first number: ");
-		first = user_input.nextDouble();
+		first = userinput.nextDouble();
 		System.out.println("Type the second number: ");
-		second = user_input.nextDouble();
+		second = userinput.nextDouble();
 		if (first > second){
 			System.out.println("The first number ("+ first +") is larger.");
+		}
 			else if (first < second){
 				System.out.println("The second number ("+ second + ") is larger.");
+			}
 				else if (first == second) {
 					System.out.println("Both numbers (" + first +" and " + second + " are equal.");
 				}
-			}
-		}
-		
 		
 	}
 }
