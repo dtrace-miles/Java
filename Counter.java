ffe797104e345336258ec1952b7a05b99f8437f2
Austin Lasota <austinlasota@gmail.com>
2014-06-19 11:58:27 -0400

    Create Counter.java

---
+++ Counter.java
@@ -0,0 +1,9 @@
+public class Counter {
+	public static void  main(String[] args) {
+		System.out.println("This program will count from one to ten.");
+		for (int number = 1; number <= 10; number++){
+			System.out.println(+ number);
+		}
+
+      }
+    }
