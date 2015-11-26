c4df5474fe630823d734aedda6472733ff4cbb00
NullSh0ck <NullSh0ck@users.noreply.github.com>
2015-11-26 17:41:46 -0500

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,13 +1,13 @@
 public class FizzBuzz {
 	public static void  main(String[] args) {
-        for (int foo = 1; foo <= 500; foo++) {
-        	if (foo % 15 == 0) {
+        for (int counter = 1; counter <= 500; counter++) {
+        	if (counter % 15 == 0) {
         		System.out.println("FizzBuzz");
         	}
-        	else if (foo % 3 == 0) {
+        	else if (counter % 3 == 0) {
         		System.out.println("Fizz");
         	}
-        	else if (foo % 5 == 0) {
+        	else if (counter % 5 == 0) {
         		System.out.println("Buzz");
         	}
          }  
